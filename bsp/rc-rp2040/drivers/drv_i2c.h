a10384a98cd605281ce4fce57897d8c25ba138db
Victor-Stefan Jach <14238510+vjach@users.noreply.github.com>
2023-01-07 11:26:37 +0100

    add i2c driver stub

---
+++ bsp/rc-rp2040/drivers/drv_i2c.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2006-2021, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author         Notes
+ * 2020-11-28     bigmagic       first version
+ */
+
+#ifndef __DRV_I2C_H__
+#define __DRV_I2C_H__
+
+#include <drivers/i2c.h>
+
+int rt_hw_i2c_init(void);
+
+#endif
