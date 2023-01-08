315b430e2f35d3534b474235583cfd938964789e
Victor-Stefan Jach <14238510+vjach@users.noreply.github.com>
2023-01-09 00:19:14 +0100

    Support both devices, handle errors

--- bsp/rc-rp2040/drivers/drv_i2c.c
+++ bsp/rc-rp2040/drivers/drv_i2c.c
@@ -24,12 +24,12 @@ struct rp2040_i2c_device_info {
   dma_channel_config rx_dma_config;
   int tx_dma_channel;
   int rx_dma_channel;
-
   struct rt_semaphore dma_completed;
+  int irq_number;
+  void (*irq_handler)();
   uint16_t dma_cmd_buffer[I2C_MAX_TX_LEN];
 };
 
-static void i2c_dma_irq_handler();
 
 static rt_err_t dma_init(struct rp2040_i2c_device_info* i2c_dev) {
     int tx_channel = dma_claim_unused_channel(false);
@@ -57,26 +57,35 @@ static rt_err_t dma_init(struct rp2040_i2c_device_info* i2c_dev) {
     channel_config_set_write_increment(&i2c_dev->rx_dma_config, true);
     channel_config_set_dreq(&i2c_dev->rx_dma_config, i2c_get_dreq(i2c_dev->dev, false));
     i2c_dev->rx_dma_channel = rx_channel;
-    //dma_channel_set_irq0_enabled(tx_channel, true);
-    //dma_channel_set_irq0_enabled(rx_channel, true);
 
     return RT_EOK;
 }
 
 static rt_err_t dma_deinit(struct rp2040_i2c_device_info* i2c_dev_info) {
-  dma_unclaim_mask((1 << i2c_dev_info->tx_dma_channel) | (1 << i2c_dev_info->rx_dma_channel));
-  irq_set_enabled(I2C0_IRQ, false);
+  uint32_t unclaim_mask = 0;
+  if (i2c_dev_info->tx_dma_channel >= 0) {
+    unclaim_mask |= 1 << i2c_dev_info->tx_dma_channel;
+  }
+
+  if (i2c_dev_info->rx_dma_channel >= 0) {
+    unclaim_mask |= 1 << i2c_dev_info->rx_dma_channel;
+  }
+
+  if (unclaim_mask) {
+    dma_unclaim_mask(unclaim_mask);
+  }
 }
 
 static rt_err_t rp2040_i2c_device_init(struct rp2040_i2c_device_info* dev_info) {
   gpio_set_function(dev_info->scl_pin, GPIO_FUNC_I2C);
   gpio_set_function(dev_info->sda_pin, GPIO_FUNC_I2C);
+  rt_sem_init(&dev_info->dma_completed, NULL, 0, 0);
   i2c_init(dev_info->dev, 100000);
-  irq_set_exclusive_handler(I2C0_IRQ, i2c_dma_irq_handler);
+  irq_set_exclusive_handler(dev_info->irq_number, dev_info->irq_handler);
+  i2c_get_hw(dev_info->dev)->dma_cr |= I2C_IC_DMA_CR_TDMAE_VALUE_ENABLED | I2C_IC_DMA_CR_RDMAE_VALUE_ENABLED;
 }
 
-
-static void fill_dma_cmd_buffer(uint16_t* dma_cmd_buffer, struct rt_i2c_msg* msg) {
+static void fill_dma_cmd_buffer(uint16_t* dma_cmd_buffer, struct rt_i2c_msg* msg, bool stop) {
   uint16_t* cmd_ptr = dma_cmd_buffer;
   if (msg->flags & RT_I2C_RD) {
     for (int i = 0; i < msg->len; ++i, ++cmd_ptr) {
@@ -88,42 +97,47 @@ static void fill_dma_cmd_buffer(uint16_t* dma_cmd_buffer, struct rt_i2c_msg* msg
     }
   }
   dma_cmd_buffer[0] |= I2C_IC_DATA_CMD_RESTART_BITS;
-  dma_cmd_buffer[msg->len - 1] |= I2C_IC_DATA_CMD_STOP_BITS;
+  if (stop) {
+      dma_cmd_buffer[msg->len - 1] |= I2C_IC_DATA_CMD_STOP_BITS;
+  }
 }
 
 static rt_size_t i2c_master_xfer(struct rt_i2c_bus_device *bus,
                                     struct rt_i2c_msg msgs[],
                                     rt_uint32_t num)
 {
+  rt_size_t ret = 0;
   struct rp2040_i2c_device_info* dev_info = bus->priv;
-  rt_sem_init(&dev_info->dma_completed, NULL, 0, 0);
   i2c_inst_t* dev = dev_info->dev;
-  dma_init(dev_info);
+  if (dma_init(dev_info) != RT_EOK) {
+    goto finish;
+  }
 
-  for (int i = 0; i < num; ++i) {
+  for (int i = 0; i < num; ++i, ++ret) {
     struct rt_i2c_msg* current_msg = &msgs[i];
-    uint32_t channel_trigger_mask = 1UL << dev_info->tx_dma_channel;
+
+    if (current_msg->len > I2C_MAX_TX_LEN) {
+      goto finish;
+    }
 
     i2c_get_hw(dev)->enable = 0;
     i2c_get_hw(dev)->tar = current_msg->addr;
     i2c_get_hw(dev)->enable = 1;
-     i2c_get_hw(dev)->intr_mask = (
-           I2C_IC_INTR_MASK_M_STOP_DET_BITS |
-               I2C_IC_INTR_MASK_M_TX_ABRT_BITS);
+    i2c_get_hw(dev)->intr_mask = (
+        I2C_IC_INTR_MASK_M_STOP_DET_BITS |
+        I2C_IC_INTR_MASK_M_TX_ABRT_BITS);
     irq_set_enabled(I2C0_IRQ, true);
 
-    // TODO: make sure data to be sent does not exceed the internal buffer
-    fill_dma_cmd_buffer(dev_info->dma_cmd_buffer, current_msg);
+    fill_dma_cmd_buffer(dev_info->dma_cmd_buffer, current_msg, true);
     dma_channel_configure(
         dev_info->tx_dma_channel, &dev_info->tx_dma_config, &i2c_get_hw(dev)->data_cmd, dev_info->dma_cmd_buffer, current_msg->len, false);
 
     if (current_msg->flags & RT_I2C_RD) {
       dma_channel_configure(
           dev_info->rx_dma_channel, &dev_info->rx_dma_config, current_msg->buf, &i2c_get_hw(dev)->data_cmd, current_msg->len, false);
-      channel_trigger_mask |= 1UL << dev_info->rx_dma_channel;
+      dma_start_channel_mask(1 << dev_info->rx_dma_channel);
     }
 
-    dma_start_channel_mask(1 << dev_info->rx_dma_channel);
     dma_start_channel_mask(1 << dev_info->tx_dma_channel);
 
     // TODO: wait until completed (with timeout)
@@ -132,10 +146,10 @@ static rt_size_t i2c_master_xfer(struct rt_i2c_bus_device *bus,
     // TODO: break the loop as soon as there is any failure
   }
 
+finish:
   dma_deinit(dev_info);
-
-  // TODO: error handling
-  return 0;
+  i2c_get_hw(dev)->enable = 0;
+  return ret;
 }
 
 static rt_size_t i2c_slave_xfer(struct rt_i2c_bus_device *bus,
@@ -159,6 +173,20 @@ static rt_err_t i2c_configure(struct rp2040_i2c_device_info *cfg)
     return RT_EOK;
 }
 
+static inline void i2c_irq_handler(struct rp2040_i2c_device_info* config) {
+  const uint32_t status = i2c_get_hw(config->dev)->intr_stat;
+
+  if (status & I2C_IC_INTR_STAT_R_TX_ABRT_BITS) {
+    // abort
+    i2c_get_hw(config->dev)->clr_tx_abrt;
+    // set a flag that transaction was aborted
+    rt_sem_release(&config->dma_completed);
+  } else if (status & I2C_IC_INTR_STAT_R_STOP_DET_BITS) {
+    i2c_get_hw(config->dev)->clr_stop_det;
+    rt_sem_release(&config->dma_completed);
+  }
+}
+
 static const struct rt_i2c_bus_device_ops rp2040_i2c_ops =
 {
     .master_xfer = i2c_master_xfer,
@@ -171,11 +199,16 @@ static const struct rt_i2c_bus_device_ops rp2040_i2c_ops =
 
 #if defined (RC_RP2040_USING_I2C0)
 #define I2C0_BUS_NAME    "i2c0"
+static void i2c0_irq_handler();
 static struct rp2040_i2c_device_info hw_device0 =
 {
   .dev = i2c0,
   .sda_pin = 4,
   .scl_pin = 5,
+  .tx_dma_channel = -1,
+  .rx_dma_channel = -1,
+  .irq_number = I2C0_IRQ,
+  .irq_handler = i2c0_irq_handler,
 };
 
 struct rt_i2c_bus_device device0 =
@@ -183,28 +216,48 @@ struct rt_i2c_bus_device device0 =
     .ops = &rp2040_i2c_ops,
     .priv =  (void *)&hw_device0,
 };
+
+static void i2c0_irq_handler() {
+  i2c_irq_handler(&hw_device0);
+}
 #endif
 
-static void i2c_dma_irq_handler() {
-  // TODO: notify xfer routine that it can proceed with the next transfer
-  struct rp2040_i2c_device_info* config = &hw_device0;
-  const uint32_t status = i2c_get_hw(config->dev)->intr_stat;
+#if defined (RC_RP2040_USING_I2C1)
+#define I2C0_BUS_NAME    "i2c1"
+static void i2c0_irq_handler();
+static struct rp2040_i2c_device_info hw_device1 =
+{
+  .dev = i2c0,
+  .sda_pin = 6,
+  .scl_pin = 7,
+  .tx_dma_channel = -1,
+  .rx_dma_channel = -1,
+  .irq_number = I2C1_IRQ,
+  .irq_handler = i2c1_irq_handler,
+};
 
-  if (status & I2C_IC_INTR_STAT_R_TX_ABRT_BITS) {
-    i2c_get_hw(config->dev)->clr_tx_abrt;
-    rt_sem_release(&config->dma_completed);
-  } else if (status & I2C_IC_INTR_STAT_R_STOP_DET_BITS) {
-    i2c_get_hw(config->dev)->clr_stop_det;
-    rt_sem_release(&config->dma_completed);
-  }
+struct rt_i2c_bus_device device1 =
+{
+    .ops = &rp2040_i2c_ops,
+    .priv =  (void *)&hw_device1,
+};
+
+static void i2c1_irq_handler() {
+  i2c_irq_handler(&hw_device0);
 }
+#endif
+
 
 int rt_hw_i2c_init(void)
 {
-    i2c_get_hw(i2c0)->dma_cr |= I2C_IC_DMA_CR_TDMAE_VALUE_ENABLED | I2C_IC_DMA_CR_RDMAE_VALUE_ENABLED;
 #if defined(RC_RP2040_USING_I2C0)
     rp2040_i2c_device_init(&hw_device0);
     rt_i2c_bus_device_register(&device0, I2C0_BUS_NAME);
+#endif
+
+#if defined(RC_RP2040_USING_I2C1)
+    rp2040_i2c_device_init(&hw_device1);
+    rt_i2c_bus_device_register(&device1, I2C1_BUS_NAME);
 #endif
 
     return 0;
