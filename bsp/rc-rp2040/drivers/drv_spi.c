6a65b2b6c9a44edafaabd36a6a17233a9b871e5a
Victor-Stefan Jach <14238510+vjach@users.noreply.github.com>
2023-01-19 20:26:36 +0100

    wip: spi all devices

--- bsp/rc-rp2040/drivers/drv_spi.c
+++ bsp/rc-rp2040/drivers/drv_spi.c
@@ -51,13 +51,31 @@ static struct rp2040_spi_hw spi0_hw_dev = {
 
 
 #if defined (BSP_USING_SPI0_DEVICE0)
-
-#define SPI0_DEVICE0_NAME  "spi0.0"
 static struct rp2040_spi_device spi0_device0 = {
-  .device_name = SPI0_DEVICE0_NAME,
+  .device_name = "can",
+  .cs_pin = 1,
+};
+#endif
+
+#if defined (BSP_USING_SPI0_DEVICE1)
+static struct rp2040_spi_device spi0_device1 = {
+  .device_name = "barometer",
+  .cs_pin = 22,
+};
+#endif
+
+#if defined (BSP_USING_SPI0_DEVICE2)
+static struct rp2040_spi_device spi0_device2 = {
+  .device_name = "compass",
   .cs_pin = 23,
 };
+#endif
 
+#if defined (BSP_USING_SPI0_DEVICE3)
+static struct rp2040_spi_device spi0_device2 = {
+  .device_name = "imu",
+  .cs_pin = 28,
+};
 #endif
 
 #endif
@@ -83,8 +101,7 @@ static rt_uint32_t rp2040_spi_xfer(struct rt_spi_device *device, struct rt_spi_m
     struct rp2040_spi_hw* hw = (struct rp2040_spi_hw *)device->bus->parent.user_data;
     struct rp2040_spi_device* internal_device = (struct rp2040_spi_device*) device->parent.user_data;
     rt_uint32_t transferred = 0;
-    //
-  // TODO: set speed
+
     for (struct rt_spi_message* current_message = message; current_message != RT_NULL; current_message = current_message->next, ++transferred) {
       if (current_message->cs_take) {
         rt_pin_write(internal_device->cs_pin, 0);
@@ -139,66 +156,22 @@ int rt_hw_spi_init(void)
 #if defined (BSP_USING_SPI0_DEVICE0)
     rp2040_spi_bus_attach_device(SPI0_BUS_NAME, &spi0_device0);
 #endif
+
+#if defined (BSP_USING_SPI0_DEVICE1)
+    rp2040_spi_bus_attach_device(SPI0_BUS_NAME, &spi0_device1);
+#endif
+
+#if defined (BSP_USING_SPI0_DEVICE2)
+    rp2040_spi_bus_attach_device(SPI0_BUS_NAME, &spi0_device2);
+#endif
+
+#if defined (BSP_USING_SPI0_DEVICE3)
+    rp2040_spi_bus_attach_device(SPI0_BUS_NAME, &spi0_device3);
+#endif
+
 #endif
     return RT_EOK;
 }
 
 INIT_DEVICE_EXPORT(rt_hw_spi_init);
-
-#if 0
-rt_err_t raspi_spi_bus_attach_device(const char *bus_name, struct raspi_spi_device *device)
-{
-    rt_err_t ret;
-    RT_ASSERT(device != RT_NULL);
-    ret = rt_spi_bus_attach_device(device->spi_device, device->device_name, bus_name, (void *)(device));
-    return ret;
-}
-
-rt_err_t raspi_spi_hw_init(struct raspi_spi_hw_config *hwcfg)
-{
-    prev_raspi_pin_mode(hwcfg->sclk_pin, hwcfg->sclk_mode);
-    prev_raspi_pin_mode(hwcfg->miso_pin, hwcfg->miso_mode);
-    prev_raspi_pin_mode(hwcfg->mosi_pin, hwcfg->mosi_mode);
-#if defined (BSP_USING_SPI0_DEVICE0)
-    prev_raspi_pin_mode(hwcfg->ce0_pin, hwcfg->ce0_mode);
 #endif
-
-#if defined (BSP_USING_SPI0_DEVICE1)
-    prev_raspi_pin_mode(hwcfg->ce1_pin, hwcfg->ce1_mode);
-#endif
-    //clear rx and tx
-    SPI_REG_CS(hwcfg->hw_base) = (SPI_CS_CLEAR_TX | SPI_CS_CLEAR_RX);
-    return RT_EOK;
-}
-
-static struct rt_spi_ops raspi_spi_ops =
-{
-    .configure = raspi_spi_configure,
-    .xfer = raspi_spi_xfer
-};
-
-struct raspi_spi_hw_config raspi_spi0_hw =
-{
-    .spi_num = 0,
-    .sclk_pin = GPIO_PIN_11,
-    .sclk_mode = ALT0,
-    .mosi_pin = GPIO_PIN_10,
-    .mosi_mode = ALT0,
-    .miso_pin = GPIO_PIN_9,
-    .miso_mode = ALT0,
-
-#if defined (BSP_USING_SPI0_DEVICE0)
-    .ce0_pin = GPIO_PIN_8,
-    .ce0_mode = ALT0,
-#endif
-
-#if defined (BSP_USING_SPI0_DEVICE1)
-    .ce1_pin = GPIO_PIN_7,
-    .ce1_mode = ALT0,
-#endif
-    .hw_base = SPI_0_BASE,
-};
-#endif
-
-#endif
-
