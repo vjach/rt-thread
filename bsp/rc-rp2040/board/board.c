d1dbeb5269771432275be6e73417a7692959c477
Victor-Stefan Jach <14238510+vjach@users.noreply.github.com>
2023-01-29 20:11:27 +0100

    pwm driver

--- bsp/rc-rp2040/board/board.c
+++ bsp/rc-rp2040/board/board.c
@@ -68,6 +68,7 @@ void rt_hw_board_init() {
   rt_hw_uart_init();
   rt_hw_sbus_init();
   rt_hw_uart1_init();
+  rt_hw_pwm_init();
 
 #ifdef RT_USING_CONSOLE
   rt_console_set_device(RT_CONSOLE_DEVICE_NAME);
