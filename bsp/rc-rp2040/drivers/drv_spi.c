87500e64bb6f56e5b9469eeff137343b74d2afb0
Victor-Stefan Jach <14238510+vjach@users.noreply.github.com>
2023-02-14 10:53:56 +0100

    spi: flow controlled spi1

--- bsp/rc-rp2040/drivers/drv_spi.c
+++ bsp/rc-rp2040/drivers/drv_spi.c
@@ -16,6 +16,7 @@
 #include "board.h"
 #include "hardware/gpio.h"
 #include "hardware/spi.h"
+#include "hardware/dma.h"
 
 // DEBUG:
 
@@ -145,6 +146,121 @@ rt_err_t rp2040_spi_device_init(struct rp2040_spi_device *device) {
   return RT_EOK;
 }
 
+#define SPI1_BUS_NAME "spi1"
+struct rp2040_spi_ext_hw {
+  struct rp2040_spi_hw hw;
+  unsigned int_pin;
+  dma_channel_config tx_dma_config;
+  dma_channel_config rx_dma_config;
+  int tx_dma_channel;
+  int rx_dma_channel;
+  struct rt_semaphore dma_completed;
+};
+
+static rt_err_t dma_init(struct rp2040_spi_ext_hw* ext_hw) {
+  struct rp2040_spi_hw* hw = &(ext_hw->hw);
+    int tx_channel = dma_claim_unused_channel(false);
+    if (tx_channel < 0) {
+      // TODO:
+        return -RT_ERROR;
+    }
+
+    ext_hw->tx_dma_config = dma_channel_get_default_config(tx_channel);
+    channel_config_set_transfer_data_size(&ext_hw->tx_dma_config, DMA_SIZE_8);
+    channel_config_set_read_increment(&ext_hw->tx_dma_config, true);
+    channel_config_set_write_increment(&ext_hw->tx_dma_config, false);
+    channel_config_set_dreq(&ext_hw->tx_dma_config, spi_get_dreq(hw->internal_bus_device, true));
+    ext_hw->tx_dma_channel = tx_channel;
+
+    int rx_channel = dma_claim_unused_channel(false);
+    if (rx_channel < 0) {
+      // TODO:
+        return -RT_ERROR;
+    }
+
+    ext_hw->rx_dma_config = dma_channel_get_default_config(rx_channel);
+    channel_config_set_transfer_data_size(&ext_hw->rx_dma_config, DMA_SIZE_8);
+    channel_config_set_read_increment(&ext_hw->rx_dma_config, false);
+    channel_config_set_write_increment(&ext_hw->rx_dma_config, true);
+    channel_config_set_dreq(&ext_hw->rx_dma_config, spi_get_dreq(hw->internal_bus_device, false));
+    ext_hw->rx_dma_channel = rx_channel;
+
+    return RT_EOK;
+}
+
+#if 0
+static rt_err_t dma_deinit(struct rp2040_i2c_device_info* i2c_dev_info) {
+  uint32_t unclaim_mask = 0;
+  if (i2c_dev_info->tx_dma_channel >= 0) {
+    unclaim_mask |= 1 << i2c_dev_info->tx_dma_channel;
+  }
+
+  if (i2c_dev_info->rx_dma_channel >= 0) {
+    unclaim_mask |= 1 << i2c_dev_info->rx_dma_channel;
+  }
+
+  if (unclaim_mask) {
+    dma_unclaim_mask(unclaim_mask);
+  }
+  return RT_EOK;
+}
+#endif
+
+static rt_uint32_t rp2040_spi_ext_xfer(struct rt_spi_device *device,
+    struct rt_spi_message *message) {
+
+  struct rp2040_spi_hw *hw =
+      (struct rp2040_spi_hw *)device->bus->parent.user_data;
+  struct rp2040_spi_ext_hw* ext_hw = rt_container_of(hw, struct rp2040_spi_ext_hw, hw);
+  // wait until mbox is empty
+  dma_channel_configure(
+      ext_hw->tx_dma_channel, &ext_hw->tx_dma_config, &spi_get_hw(hw->internal_bus_device)->dr, message->send_buf, message->length, false);
+
+  dma_channel_configure(
+      ext_hw->rx_dma_channel, &ext_hw->rx_dma_config, message->recv_buf, &spi_get_hw(hw->internal_bus_device)->dr, message->length, false);
+  dma_start_channel_mask(1 << ext_hw->rx_dma_channel);
+
+
+  dma_start_channel_mask((1 << ext_hw->tx_dma_channel) | (1 << ext_hw->rx_dma_channel));
+  rt_pin_write(ext_hw->int_pin, 1);
+
+  // TODO: do that in interrupt
+  //rt_sem_take(&ext_hw->dma_completed, RT_WAITING_FOREVER);
+  dma_channel_wait_for_finish_blocking(ext_hw->rx_dma_channel);
+  rt_pin_write(ext_hw->int_pin, 0);
+  return 1;
+}
+
+static struct rt_spi_ops rp2040_spi_ext_ops = {.configure = rp2040_spi_configure,
+                                           .xfer = rp2040_spi_ext_xfer};
+
+static struct rp2040_spi_ext_hw spi1_hw_dev = {
+  {
+    .internal_bus_device = spi1,
+    .baudrate = 5000000,
+    .miso_pin = 11,
+    .mosi_pin = 8,
+    .sck_pin = 3,
+  },
+  .int_pin = 12,
+};
+
+rt_err_t rp2040_spi_ext_hw_init(struct rp2040_spi_ext_hw *ext_hw) {
+  struct rp2040_spi_hw* hw = &(ext_hw->hw);
+  rp2040_spi_hw_init(hw);
+  spi_set_slave(hw->internal_bus_device, true);
+  rt_pin_mode(ext_hw->int_pin, PIN_MODE_OUTPUT);
+  rt_pin_write(ext_hw->int_pin, 0);
+  dma_init(ext_hw);
+  return RT_EOK;
+}
+
+static struct rp2040_spi_device spi1_device0 = {
+    .device_name = "app_ctrl",
+    // unused
+    .cs_pin = -1,
+};
+
 int rt_hw_spi_init(void) {
 #if defined(BSP_USING_SPI0_BUS)
   rp2040_spi_hw_init(&spi0_hw_dev);
@@ -168,6 +284,11 @@ int rt_hw_spi_init(void) {
 #endif
 
 #endif
+
+  rp2040_spi_ext_hw_init(&spi1_hw_dev);
+  rt_spi_bus_register(&spi1_hw_dev.hw.bus_device, SPI1_BUS_NAME, &rp2040_spi_ext_ops);
+  spi1_hw_dev.hw.bus_device.parent.user_data = &spi1_hw_dev;
+  rp2040_spi_bus_attach_device(SPI1_BUS_NAME, &spi1_device0);
   return RT_EOK;
 }
 
