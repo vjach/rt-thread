faa8c904fe2b08b63f08a66b24a1cf900cad83f3
Victor-Stefan Jach <14238510+vjach@users.noreply.github.com>
2023-02-14 19:15:14 +0100

    gpio: attach irq

--- bsp/rc-rp2040/drivers/drv_gpio.c
+++ bsp/rc-rp2040/drivers/drv_gpio.c
@@ -8,7 +8,22 @@
  * 2021-01-28     flybreak       first version
  */
 
+#include <string.h>
 #include "drv_gpio.h"
+
+typedef void (*irq_handler)(void *args);
+typedef struct {
+  irq_handler handler;
+  void *cookie;
+} irq_entry;
+
+static irq_entry pin_irq_entry[25];
+
+void pico_gpio_handler(unsigned gpio, uint32_t events) {
+  if (pin_irq_entry[gpio].handler != NULL) {
+    pin_irq_entry[gpio].handler(pin_irq_entry[gpio].cookie);
+  }
+}
 
 static void pico_pin_mode(struct rt_device *dev, rt_base_t pin, rt_base_t mode)
 {
@@ -47,12 +62,20 @@ static int pico_pin_read(struct rt_device *device, rt_base_t pin)
     return (gpio_get(pin)? PIN_HIGH : PIN_LOW);
 }
 
+static rt_err_t pin_attach_irq(struct rt_device *device, rt_int32_t pin, rt_uint32_t mode, void (*hdr)(void *args),
+    void *args) {
+  pin_irq_entry[pin].handler = hdr;
+  pin_irq_entry[pin].cookie = args;
+  gpio_set_irq_enabled_with_callback(pin, GPIO_IRQ_EDGE_RISE, true, pico_gpio_handler);
+  return RT_EOK;
+}
+
 static const struct rt_pin_ops ops =
 {
     pico_pin_mode,
     pico_pin_write,
     pico_pin_read,
-    RT_NULL,
+    pin_attach_irq,
     RT_NULL,
     RT_NULL,
     RT_NULL,
@@ -61,7 +84,7 @@ static const struct rt_pin_ops ops =
 int rt_hw_gpio_init(void)
 {
     rt_device_pin_register("gpio", &ops, RT_NULL);
-
+    memset(pin_irq_entry, 0, sizeof(pin_irq_entry));
     return 0;
 }
 INIT_DEVICE_EXPORT(rt_hw_gpio_init);
